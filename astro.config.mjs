4e8b4efcbf02ffe4ca23d1ea1de16be964425a36
Muhammad Farhan Abiyyullah <farhanabiyullah@gmail.com>
2022-10-01 16:29:16 +0700

    App: Create blog section & configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-import preact from '@astrojs/preact';
-import tailwind from '@astrojs/tailwind';
 import image from '@astrojs/image';
+import mdx from '@astrojs/mdx';
+import preact from '@astrojs/preact';
 import partytown from '@astrojs/partytown';
+import tailwind from '@astrojs/tailwind';
+
+import { remarkReadingTime } from './src/lib/remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,10 +13,14 @@ export default defineConfig({
     preact(),
     tailwind(),
     image(),
+    mdx(),
     partytown({
       config: {
         forward: ['dataLayer.push'],
       },
     }),
   ],
+  markdown: {
+    remarkPlugins: [remarkReadingTime],
+  },
 });
