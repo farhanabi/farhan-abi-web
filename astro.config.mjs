316aeb0da206a5fcc1ca436d7a1f653ec85a3dc7
Muhammad Farhan Abiyyullah <farhanabiyullah@gmail.com>
2022-10-06 20:49:05 +0700

    App: Styling tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,6 +9,7 @@ import { remarkReadingTime } from './src/lib/remark-reading-time.mjs';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://farhanabi.com',
   integrations: [
     preact(),
     tailwind(),
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,22 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        blink: 'blink 1s step-end infinite',
+        'fade-out': 'fadeOut 10s ease-out infinite',
+      },
+      keyframes: {
+        blink: {
+          '0%, 100%': { 'border-color': 'white' },
+          '50%': { 'border-color': 'black' },
+        },
+        fadeOut: {
+          '0%, 33%, 100%': { opacity: 1 },
+          '35%, 98%': { opacity: 0 },
+        },
+      },
+    },
   },
   plugins: [],
 };
