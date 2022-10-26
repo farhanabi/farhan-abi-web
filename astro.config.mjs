3f5c125d411c768885716d26d30ab98e176c7e2a
Muhammad Farhan Abiyyullah <farhanabiyullah@gmail.com>
2022-10-26 17:38:49 +0700

    App: Performance improvement

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import preact from '@astrojs/preact';
-import partytown from '@astrojs/partytown';
 import tailwind from '@astrojs/tailwind';
 
 import { remarkReadingTime } from './src/lib/remark-reading-time.mjs';
@@ -10,17 +9,7 @@ import { remarkReadingTime } from './src/lib/remark-reading-time.mjs';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://farhanabi.com',
-  integrations: [
-    preact(),
-    tailwind(),
-    image(),
-    mdx(),
-    partytown({
-      config: {
-        forward: ['dataLayer.push'],
-      },
-    }),
-  ],
+  integrations: [preact(), tailwind(), image(), mdx()],
   markdown: {
     remarkPlugins: [remarkReadingTime],
   },
